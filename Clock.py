835a4f8e24dc5e9bb93898756bb18fdd6639b2d1
Thomass5729 <tdeschulthess@outlook.com>
2024-11-17 18:44:30 +0000

    Add files via upload

--- Clock.py
+++ Clock.py
@@ -3,15 +3,17 @@
 from tkinter import *
 from tkinter.ttk import *
 from time import strftime
+import random
 
 win = Tk()
-win.title("Clock")
+win.title("My digital clock")
 
 def time():
     currenttime = strftime("%d/%m/%Y %H:%M:%S %p")
-
-    clocklabel.config(text=currenttime)
+    color_list = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'brown', 'gray', 'gold', 'cyan', 'Gainsboro', 'gray', 'dimgray', 'LightSlateGray','AliceBlue', 'LimeGreen', 'DarkKhaki', 'Khaki']
+    clocklabel.config(text=currenttime,background=random.choice(color_list),foreground=random.choice(color_list))
     clocklabel.after(1000,time)
+
 
 
 clocklabel = Label(win,font=("arial",50,"bold"),background="grey",foreground="red")
